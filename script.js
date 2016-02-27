bbf06afaf35e8998f6ac75b910e9de74e5435925
Olesya <kolesya89@gmail.com>
2016-02-27 14:28:49 +0000

    add this page

---
+++ script.js
@@ -0,0 +1,36 @@
+// JavaScript File
+/*var money;
+money = prompt("How much money do you have?");
+if(money >= 100) {
+  alert("go shoping") 
+} else {
+    alert("earn money")
+    
+}
+*/
+/*for(var i = 1; i <= 10; i++) {
+    alert("Olesya is smart="+i);
+}
+*/
+// var i = 1;
+// while(i <= 10) {
+//     alert("Olesya is smart=" + i);
+//     i++;
+// }
+
+// var i = 0;
+// var j = 0;
+// do {
+//   i += 5;
+//   j++;
+//   document.write("<p>Olesya is smart=</p>"+j)
+// } while (i < 100)
+
+
+var sum = 0;
+for(var n = 1; n <= 10; n++) {
+    sum = sum+n;
+    document.write(sum + "<br>");
+}
+
+document.write(sum + "<br>");
